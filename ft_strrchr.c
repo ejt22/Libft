040c051defad7370a8d51e1f8bdc6e067fdd9ea1
ejt22 <100273687+ejt22@users.noreply.github.com>
2022-04-21 12:20:02 +0200

    Add files via upload

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ejoo-tho <ejoo-tho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 15:14:47 by ejoo-tho          #+#    #+#             */
-/*   Updated: 2022/04/06 12:55:53 by ejoo-tho         ###   ########.fr       */
+/*   Updated: 2022/04/21 12:17:43 by ejoo-tho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 */
+
 char	*ft_strrchr(const char *s, int c)
 {
 	size_t	i;
@@ -32,7 +33,7 @@ char	*ft_strrchr(const char *s, int c)
 	if (!s)
 		return (NULL);
 	len = ft_strlen(s);
-	i = len - 1;
+	i = len;
 	while (i > 0 && s[i] != ((char)c))
 		i--;
 	if (s[i] == ((char)c))
@@ -44,10 +45,11 @@ char	*ft_strrchr(const char *s, int c)
 
 int	main()
 {
-	char	s[] = "Hellocharlie";
-	int		c = 'e';
+	char	s[] = "bonjour";
+	int		c = '\0';
 
 	printf("%s\n", ft_strrchr(s, c));
+	printf("%s\n", strrchr(s, c));
 	return (0);
 }
 */
