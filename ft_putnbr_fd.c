aa78866364b89b7b8b9eacc4bee9e6c56ed31880
ejt22 <100273687+ejt22@users.noreply.github.com>
2022-04-21 13:53:48 +0200

    Add files via upload

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,17 +6,31 @@
 /*   By: ejoo-tho <ejoo-tho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 14:12:15 by ejoo-tho          #+#    #+#             */
-/*   Updated: 2022/04/19 15:08:05 by ejoo-tho         ###   ########.fr       */
+/*   Updated: 2022/04/21 13:52:47 by ejoo-tho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 /*
 #include <unistd.h>
 
 void	ft_putchar_fd(char c, int fd)
 {
 	write (fd, &c, 1);
+}
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	i;
+
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		ft_putchar_fd(s[i], fd);
+		i++;
+	}
 }
 */
 void	ft_putnbr_fd(int n, int fd)
@@ -35,6 +49,14 @@ void	ft_putnbr_fd(int n, int fd)
 	else
 	{
 		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % n, fd);
+		ft_putnbr_fd(n % 10, fd);
 	}
 }
+
+/*
+int	main(void)
+{
+	ft_putnbr_fd(42, 1);
+	return (0);
+}
+*/
